5068621603303018cd6c21d191da08b7250dae37
sharmavaibhav31 <sharma31stmay@gmail.com>
2026-01-17 17:35:20 +0530

    Conditional Logic Group 2

---
+++ conditionalLogicB.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+/*
+Questions:
+(1) Even / Odd (without %)
+(6) Divisible by 3 and 5
+(12) Ends with 5 or 0
+(13) Same last digit
+(14) Multiple of another number
+(16) Divisible by 4 but not by 8
+*/
+
+public class conditionalLogicB {
+    
+}

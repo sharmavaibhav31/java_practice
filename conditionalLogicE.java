7552188c3c4398bd4622c4eb0dfdf09e8f62e324
sharmavaibhav31 <sharma31stmay@gmail.com>
2026-01-17 21:29:11 +0530

    Conditional Logic Group 5

--- conditionalLogicE.java
+++ conditionalLogicE.java
@@ -10,7 +10,6 @@ public class conditionalLogicE {
    (17) Absolute value (no library)
    (19) String compare (manual)
    */
-
    public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
 
@@ -33,5 +32,35 @@ public class conditionalLogicE {
     else{
         System.out.println("Triangle is Invalid");
     }
+
+    // To find absolute value without a library
+
+    int num = sc.nextInt();
+    if(num < 0){
+        num = -num;
+        System.out.println("Abs of Num is " + num);
+    }
+    else{
+        System.out.println("Abs of Num is " + num);
+    }
+
+    String str1 = sc.next();
+    String str2 = sc.next();
+
+    if(str1.length() != str2.length()) {
+        System.out.println("String length unmatched");
+    }
+    
+    for(int i = 0; i < str1.length(); i++){
+        if(str1.charAt(i) != str2.charAt(i)){
+            System.out.println("Strings not matched");
+        }
+        else{
+            System.out.println("Strings Matched");
+        }
+    }
+
+
+
    }
 }

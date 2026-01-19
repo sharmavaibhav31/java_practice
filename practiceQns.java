93acb2a0303419af13810e21df05c832f0f0f2ba
sharmavaibhav31 <sharma31stmay@gmail.com>
2026-01-19 14:01:56 +0530

    Qn 13

--- practiceQns.java
+++ practiceQns.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class practiceQns {
     
     //given an integer array, move all zeros to the end.
@@ -12,9 +14,24 @@ public class practiceQns {
         while (count < n) {
             arr[count++] = 0;
         }
+        System.out.println("Array is: ");
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i]);
+        }
     }
 
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array size: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the array elements: ");
+        int[] arr = new int[a];
+        for(int i = 0; i < a; i++){
+            arr[i] = sc.nextInt();
+        }
+        movingZeros(arr);
+
+
 
     }
 }

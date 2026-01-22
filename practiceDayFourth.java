982b9a97ddec237365ac21c01a7a5272063d875f
sharmavaibhav31 <sharma31stmay@gmail.com>
2026-01-22 12:19:38 +0530

    Day 4 Practice

--- practiceDayFourth.java
+++ practiceDayFourth.java
@@ -1,4 +1,14 @@
 public class practiceDayFourth {
-    
+
+    //5. Given two strings, check if the second string is a rotation of the first string.
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()){
+            return false;
+        }
+        else{
+        String newStr = s + s;
+        return newStr.contains(goal);
+        }
+    }
     
 }

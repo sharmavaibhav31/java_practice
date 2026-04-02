d76ea6a1758e09f1af967cab91d2d38e22ed0fa0
sharmavaibhav31 <sharma31stmay@gmail.com>
2026-04-02 08:10:10 +0530

    HashMaps

--- CollectionsPracatice.java
+++ CollectionsPracatice.java
@@ -1,19 +1,29 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CollectionsPracatice {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        ArrayList<Integer> list1 = new ArrayList<Integer>();
-        ArrayList<Integer> list2 = new ArrayList<Integer>();
-        list1.add(1);
-        list1.add(2);
-        list1.add(3);
-        list2.add(11);
-        list2.add(12);
-        list2.add(13);
-        list1.addAll(list2);
-        System.out.println(list1);
-        list2.clear();
-        System.out.println(list2);
+        // System.out.println("Hello World");
+        // ArrayList<Integer> list1 = new ArrayList<Integer>();
+        // ArrayList<Integer> list2 = new ArrayList<Integer>();
+        // list1.add(1);
+        // list1.add(2);
+        // list1.add(3);
+        // list2.add(11);
+        // list2.add(12);
+        // list2.add(13);
+        // list1.addAll(list2);
+        // System.out.println(list1);
+        // list2.clear();
+        // System.out.println(list2);
+
+        //hashmaps
+        HashMap<Integer, String>map = new HashMap<>();
+        //Hashmap is not a part of collections, it is a part of maps
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        System.out.println(map);
+
     }
 }

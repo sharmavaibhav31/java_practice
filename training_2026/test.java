81a59925d040fe0631690ff659458cc0afd686ec
sharmavaibhav31 <sharma31stmay@gmail.com>
2026-07-15 16:07:42 +0530

    test

--- training_2026/test.java
+++ training_2026/test.java
@@ -14,18 +14,24 @@ public class test {
           ****
          *****  
             */
-        int n = 5;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n - i; j++){
-                System.out.print(" ");
-            }
-            for(int j = 0; j < i + 1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
+        // int n = 5;
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < n - i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 0; j < i + 1; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
             // String spaces = " ".repeat(n - i - 1);
             // String stars = "*".repeat(i + 1);
             // System.out.println(spaces + stars);
+        String str = "MITE is best in India";
+        str = "MITE is bestest in India";
+        System.out.println(str);
+        String str2 = "ABC";
+        String str3 = "ABC";
+        String str4 = "XYZ";
+
         }
     }
-}

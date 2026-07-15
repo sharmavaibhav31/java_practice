c1ae228694e299d19adbae964ad243275fb60e0e
sharmavaibhav31 <sharma31stmay@gmail.com>
2026-07-15 13:17:25 +0530

    patterns logic

---
+++ training_2026/test.java
@@ -0,0 +1,31 @@
+package training_2026;
+
+public class test {
+    public static void main(String[] args) {
+        // for(int i = 5; i >= 1; i--){
+        //     String stars = " *".repeat(i);
+        //     System.out.println(stars);
+        // }
+
+        /*print pattern
+             *
+            ** 
+           ***
+          ****
+         *****  
+            */
+        int n = 5;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n - i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < i + 1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            // String spaces = " ".repeat(n - i - 1);
+            // String stars = "*".repeat(i + 1);
+            // System.out.println(spaces + stars);
+        }
+    }
+}

3867a31c4fd1ceec3c676aea9ea3e70962cb7177
sharmavaibhav31 <sharma31stmay@gmail.com>
2026-07-20 01:28:59 +0530

    Collections

--- CollectionsPracatice.java
+++ CollectionsPracatice.java
@@ -3,19 +3,20 @@ import java.util.HashMap;
 
 public class CollectionsPracatice {
     public static void main(String[] args) {
-        // System.out.println("Hello World");
-        // ArrayList<Integer> list1 = new ArrayList<Integer>();
-        // ArrayList<Integer> list2 = new ArrayList<Integer>();
-        // list1.add(1);
-        // list1.add(2);
-        // list1.add(3);
-        // list2.add(11);
-        // list2.add(12);
-        // list2.add(13);
-        // list1.addAll(list2);
-        // System.out.println(list1);
-        // list2.clear();
-        // System.out.println(list2);
+        System.out.println("Hello World");
+        ArrayList obj = new ArrayList<>(); 
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list2.add(11);
+        list2.add(12);
+        list2.add(13);
+        list1.addAll(list2);
+        System.out.println(list1);
+        list2.clear();
+        System.out.println(list2);
 
         //hashmaps
         HashMap<Integer, String>map = new HashMap<>();
